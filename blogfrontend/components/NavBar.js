d3ea2fd28346856a7b2e68bb2506917c75376835
catalin.festila <catafest76@gmail.com>
2022-05-20 11:42:51 +0300

    create folder components and add files

---
+++ blogfrontend/components/NavBar.js
@@ -0,0 +1,6 @@
+const NavBar = () => {
+  return (
+  <nav>Nav Bar</nav>
+  )
+}
+export default NavBar
--- blogfrontend/pages/_app.js
+++ blogfrontend/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import NavBar from '../components/NavBar'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <div className="app-container">
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
